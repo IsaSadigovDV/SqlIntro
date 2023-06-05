9e1d700d1fc6dc91681d7a6f7502a2dc3341e2a1
IsaSadigovDV <isans@code.edu.az>
2023-06-05 19:04:41 +0400

    done

---
+++ introductionTask.sql
@@ -0,0 +1,27 @@
+CREATE DATABASE Company
+USE Company
+--CREATE TABLE Department
+--(
+--	id int PRIMARY KEY,
+--	[name] nvarchar(20)
+--)
+
+--ALTER TABLE Department
+--    ADD 
+--        employeeCount VARCHAR (50) NOT NULL;
+
+
+--INSERT INTO Department Values(2, 'Ilkin', '99'),
+--(3, 'Dadashow', '98.85')
+
+	SELECT id FROM Department
+	DELETE FROM Department where id=1
+	UPDATE Department SET [name]='ilkane' Where Id=2
+
+
+
+
+
+
+
+
